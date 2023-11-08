bfdd2dbeec56af87ecbd0bd08fb1a87235d77034
RobinBIDOT <134970212+RobinBIDOT@users.noreply.github.com>
2023-11-08 13:46:17 +0100

    Add files via upload

--- robin/script.js
+++ robin/script.js
@@ -1,11 +1,68 @@
 // Définition des variables et des constantes :
+
+// Pour cleaner le HTML :
+const nombresEssais = 6;
+const nombresLettres = 5;
+let numberOfTry = 0;
+
 const elementsTouches = document.querySelectorAll("button");
 let ligne = 1;
 let lettre = 1;
 const motMystere = "super";
 const elementsMot = document.querySelectorAll(".ligneMot")
+let tempsRestant = 300;
+const elementChrono = document.getElementById('chrono');
 let finJeu = false;
 let win = false;
+let gameOver = false;
+
+
+
+const boutonOuvrir = document.getElementById('ouvrirRegles');
+const modal = document.getElementById('maModal');
+const boutonFermer = document.querySelector('.fermer');
+
+document.getElementById('nombreEssaisAutorises').textContent = nombresEssais;
+
+boutonOuvrir.addEventListener('click', function() {
+    modal.style.display = 'block';
+});
+
+boutonFermer.addEventListener('click', function() {
+    modal.style.display = 'none';
+});
+
+
+window.addEventListener('click', function(event) {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+});
+
+// Pour cleaner le HTML :
+// Fonction pour générer la grille en fonction du nombre d'essais et de lettres :
+function genererGrille(nombresEssais, nombresLettres) {
+    let parent = document.querySelector('.motsJoueur');
+    let conteneurMots = document.createElement('div');
+    conteneurMots.classList.add('conteneurMots');
+
+    for (let i = 0; i < nombresEssais; i++) {
+        let ligneMot = document.createElement('div');
+        ligneMot.classList.add('ligneMot');
+
+        for (let j = 0; j < nombresLettres; j++) {
+            let mot = document.createElement('div');
+            mot.classList.add('mot');
+            ligneMot.append(mot);
+        }
+        conteneurMots.append(ligneMot);
+    }
+    parent.append(conteneurMots);
+}
+genererGrille(nombresEssais, nombresLettres);
+// const resultatGrille = genererGrille(nombresEssais, nombresLettres);
+// const divConteneurMots = document.querySelector('.conteneurMots');
+// divConteneurMots.appendChild(resultatGrille);
 
 // Récupération via un forEach des valeurs des touches du clavier :
 elementsTouches.forEach((element) => {
@@ -59,6 +116,10 @@ function confirmerSaisieMot () {
         verifierMot ();
         ligne += 1;
         lettre = 1;
+        numberOfTry += 1;
+
+        // Mettre à jour l'affichage :
+        document.getElementById('nombresEssais').textContent = numberOfTry;
     }
 }
 
@@ -93,6 +154,32 @@ function pressionTouche (clef) {
     // else....   
 }
 
+// Fonction pour mettre à jour et afficher le chrono
+function actualiserChrono() {
+    const minutes = Math.floor(tempsRestant / 60);
+    let secondes = tempsRestant % 60;
 
+    // Ajoute un zéro devant les secondes si elles sont inférieures à 10
+    if (secondes < 10) {
+        secondes = '0' + secondes;
+    }
+    // Affichage du chrono
+    elementChrono.innerHTML = `<i class="fa-solid fa-clock"></i><br><br> ${minutes}:${secondes}`;
 
+    // Décrémente le temps
+    tempsRestant--;
+
+    // Si le temps est écoulé
+    if (tempsRestant < 0) {
+        finJeu = true;
+        gameOver = true;
+        // alert('Perdu ! Le mot mystère était : ' + motMystere); pour contrôle
+        clearInterval(chrono);
+        elementChrono.innerHTML = "Temps écoulé ! Game Over";
+        // Autres actions à effectuer une fois le temps écoulé
+        // Par exemple : gameOver = true; ou exécuter une fonction gameOver()
+    }
+}
+// Appel de la fonction toutes les 1 seconde pour mettre à jour le chrono
+const chrono = setInterval(actualiserChrono, 1000);
 
