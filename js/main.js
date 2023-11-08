5bcf482228875c9e1a8b252286b8eb5f5e1dc1ad
RomainV <romain.vorler@gmail.com>
2023-11-08 13:59:04 +0100

    réorganisation dossier de travail

---
+++ js/main.js
@@ -0,0 +1,20 @@
+import {allWords} from './words.js'
+import './control_function.js'
+import './keyboard.js'
+
+
+const letters = [
+    "A", "Z", "E", "R", "T", "Y", "U", "I", "O", "P",
+    "Q", "S", "D", "F", "G", "H", "J", "K", "L", "M",
+    "W", "X", "C", "V", "B", "N"
+]
+const words = allWords()
+const enter = "ENTER"
+const backspace = "◄"
+var userProposition = []
+var numberLetters = 5
+const nombreEssais = 6
+const nombreLettres = 5
+
+genererGrille(nombreEssais, nombreLettres)
+displayLetter()
