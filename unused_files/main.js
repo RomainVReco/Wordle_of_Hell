c7992e31159033bd35f2c1cc48dd42d9713efc49
RomainV <romain.vorler@gmail.com>
2023-11-09 11:50:36 +0100

    ecran de jeu

---
+++ unused_files/main.js
@@ -0,0 +1,21 @@
+// // import * as maGrille from './js/grilleJeu.js'
+// import * as monKeyboard from './js/keyboard.js'
+// import * as monChrono from './js/chrono.js'
+// import * as lesRegles from './js/rules.js'
+
+
+// const letters = [
+//     "A", "Z", "E", "R", "T", "Y", "U", "I", "O", "P",
+//     "Q", "S", "D", "F", "G", "H", "J", "K", "L", "M",
+//     "W", "X", "C", "V", "B", "N"
+// ]
+// const words = allWords()
+// const nombreEssais = 6
+// const nombreLettres = 5
+
+
+// // genererGrille(nombreEssais, nombreLettres)
+// // displayKeyboard() 
+
+// genererGrille(nombreEssais, nombreLettres);
+
