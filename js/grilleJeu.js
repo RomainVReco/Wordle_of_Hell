a2fc767ed0de6f3589544b37c133d0f861984b76
RomainV <romain.vorler@gmail.com>
2023-11-09 13:06:59 +0100

    grille + clavier et composants

--- js/grilleJeu.js
+++ js/grilleJeu.js
@@ -1,43 +1,16 @@
+import * as configWordle from './configWordle.js'
 // Définition des variables et des constantes :
 
 // Pour cleaner le HTML :
-const nombresEssais = 6;
-const nombresLettres = 5;
-let numberOfTry = 0;
+const nombresEssais = configWordle.getNomnbreEssais();
+const nombresLettres = configWordle.getNumberOfLetters();
 
-const elementsTouches = document.querySelectorAll("button");
 let ligne = 1;
 let lettre = 1;
 const motMystere = "super";
 const elementsMot = document.querySelectorAll(".ligneMot")
-let tempsRestant = 300;
-const elementChrono = document.getElementById('chrono');
-let finJeu = false;
-let win = false;
-let gameOver = false;
-
-
-
-const boutonOuvrir = document.getElementById('ouvrirRegles');
-const modal = document.getElementById('maModal');
-const boutonFermer = document.querySelector('.fermer');
 
 document.getElementById('nombreEssaisAutorises').textContent = nombresEssais;
-
-boutonOuvrir.addEventListener('click', function() {
-    modal.style.display = 'block';
-});
-
-boutonFermer.addEventListener('click', function() {
-    modal.style.display = 'none';
-});
-
-
-window.addEventListener('click', function(event) {
-    if (event.target === modal) {
-        modal.style.display = 'none';
-    }
-});
 
 // Pour cleaner le HTML :
 // Fonction pour générer la grille en fonction du nombre d'essais et de lettres :
@@ -47,10 +20,11 @@ function genererGrille(nombresEssais, nombresLettres) {
     conteneurMots.classList.add('conteneurMots');
 
     for (let i = 0; i < nombresEssais; i++) {
+        console.log("je suis dans la ligne")
         let ligneMot = document.createElement('div');
         ligneMot.classList.add('ligneMot');
-
         for (let j = 0; j < nombresLettres; j++) {
+            console.log("je suis dans la colonne")
             let mot = document.createElement('div');
             mot.classList.add('mot');
             ligneMot.append(mot);
@@ -60,18 +34,6 @@ function genererGrille(nombresEssais, nombresLettres) {
     parent.append(conteneurMots);
 }
 genererGrille(nombresEssais, nombresLettres);
-// const resultatGrille = genererGrille(nombresEssais, nombresLettres);
-// const divConteneurMots = document.querySelector('.conteneurMots');
-// divConteneurMots.appendChild(resultatGrille);
-
-// Récupération via un forEach des valeurs des touches du clavier :
-elementsTouches.forEach((element) => {
-    element.addEventListener("click", function() {
-        pressionTouche (element.attributes["data-key"].value)
-    });
-});
-
-
 
 function remplirMot (clef) {
     // console.log(elementsMot); pour contrôle console
@@ -108,78 +70,3 @@ function verifierMot () {
         // Faire la bascule sur l'écran game over
     }
 }
-
-function confirmerSaisieMot () {
-    if(lettre < 6) {
-        alert("Pas assez de lettres");
-    } else {
-        verifierMot ();
-        ligne += 1;
-        lettre = 1;
-        numberOfTry += 1;
-
-        // Mettre à jour l'affichage :
-        document.getElementById('nombresEssais').textContent = numberOfTry;
-    }
-}
-
-function effacerLettre () {
-    const elementsLettre = elementsMot[ligne - 1].querySelectorAll(".mot");
-    // console.log(elementsLettre); pour contrôle console
-
-    for (let index = elementsLettre.length -1; index >= 0; index--) {
-        // console.log(elementsLettre[index].innerText); pour contrôle console
-        const element = elementsLettre[index];
-        if (element.innerText !== '') {
-            element.innerText = '';
-            lettre -= 1;
-            break;
-        }
-    }
-}
-
-
-// Fonction pour les touches du clavier :
-function pressionTouche (clef) {
-    // console.log(clef); pour contrôle console
-    if (!finJeu) {
-        if(clef.toLowerCase() === 'enter') {
-            confirmerSaisieMot ();
-        } else if (clef.toLowerCase() === 'del') {
-            effacerLettre ();
-        } else {
-            remplirMot (clef);
-        }
-    }
-    // else....   
-}
-
-// Fonction pour mettre à jour et afficher le chrono
-function actualiserChrono() {
-    const minutes = Math.floor(tempsRestant / 60);
-    let secondes = tempsRestant % 60;
-
-    // Ajoute un zéro devant les secondes si elles sont inférieures à 10
-    if (secondes < 10) {
-        secondes = '0' + secondes;
-    }
-    // Affichage du chrono
-    elementChrono.innerHTML = `<i class="fa-solid fa-clock"></i><br><br> ${minutes}:${secondes}`;
-
-    // Décrémente le temps
-    tempsRestant--;
-
-    // Si le temps est écoulé
-    if (tempsRestant < 0) {
-        finJeu = true;
-        gameOver = true;
-        // alert('Perdu ! Le mot mystère était : ' + motMystere); pour contrôle
-        clearInterval(chrono);
-        elementChrono.innerHTML = "Temps écoulé ! Game Over";
-        // Autres actions à effectuer une fois le temps écoulé
-        // Par exemple : gameOver = true; ou exécuter une fonction gameOver()
-    }
-}
-// Appel de la fonction toutes les 1 seconde pour mettre à jour le chrono
-const chrono = setInterval(actualiserChrono, 1000);
-
