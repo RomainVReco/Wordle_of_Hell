c28906ad3397ee992b708769922325b45987cb59
RomainV <romain.vorler@gmail.com>
2023-11-09 13:06:59 +0100

    grille + clavier et composants

---
+++ js/configWordle.js
@@ -0,0 +1,31 @@
+import {allWords} from './words.js'
+
+const words = allWords()
+const nombreEssais = 6
+const time = 300
+const numberOfLines = nombreEssais
+const numberOfLetters = 5;
+
+let shuffleInt = Math.floor(Math.random() * words.length);
+const motMystere = words[shuffleInt]
+console.log("Mot mystère : "+motMystere)
+
+export function getNomnbreEssais(){
+    return nombreEssais
+}
+
+export function getTime(){
+    return time
+}
+
+export function getNumberOfLines(){
+    return numberOfLines
+}
+
+export function getNumberOfLetters(){
+    return numberOfLetters
+}
+
+export function getMotMystere() {
+    return motMystere
+}
